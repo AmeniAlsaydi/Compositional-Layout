d1b807f35b7c233763e20ad3d3acd5baa08544d1
Ameni Alsaydi <amenialsaydi@pursuit.org>
2020-08-24 11:49:51 -0400

    added section titles

--- Nested-Groups- Orthonognal-Scrolling/Nested-Groups- Orthonognal-Scrolling/ViewController.swift
+++ Nested-Groups- Orthonognal-Scrolling/Nested-Groups- Orthonognal-Scrolling/ViewController.swift
@@ -26,11 +26,22 @@ enum SectionKind: Int, CaseIterable {
     var nestedGroupHeight: NSCollectionLayoutDimension {
         switch self {
         case .first:
-            return .fractionalWidth(1.0)
+            return .fractionalWidth(0.9)
         default:
-            return .fractionalWidth(0.5)
+            return .fractionalWidth(0.45)
         }
     }
+    
+    var sectionTitle: String {
+        switch self {
+        case .first:
+            return "First Section"
+        case .second:
+            return "Second Section"
+        case .third:
+            return "Third Section"
+    }
+}
 }
 
 class ViewController: UIViewController {
@@ -45,6 +56,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         configureCollectionView()
         configureDataSource()
+        navigationItem.title = "Nested Groups and Orthogonal Scrolling"
     }
 
     private func configureCollectionView() {
@@ -52,6 +64,7 @@ class ViewController: UIViewController {
         collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createLayout())
         collectionView.backgroundColor = .systemGray
         collectionView.register(LabelCell.self, forCellWithReuseIdentifier: LabelCell.reuseIdentifier)
+        collectionView.register(HeaderView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: HeaderView.reuseIdentifier)
         collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         view.addSubview(collectionView)
     }
@@ -76,16 +89,28 @@ class ViewController: UIViewController {
             // item
             let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
             let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            let itemSpacing: CGFloat = 5
+            item.contentInsets = NSDirectionalEdgeInsets(top: itemSpacing, leading: itemSpacing, bottom: itemSpacing, trailing: itemSpacing)
             
             // group
             let innerGroupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.50), heightDimension: .fractionalHeight(1.0))
             let innerGroup = NSCollectionLayoutGroup.vertical(layoutSize: innerGroupSize, subitem: item, count: sectionKind.itemCount )
             
-            let nestedGroupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: sectionKind.nestedGroupHeight) // we want square so we want full width of 1/2 width
+            let nestedGroupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.9), heightDimension: sectionKind.nestedGroupHeight) // we want square so we want full width of 1/2 width
             let nestedGroup = NSCollectionLayoutGroup.horizontal(layoutSize: nestedGroupSize, subitems: [innerGroup])
             
             // section
             let section = NSCollectionLayoutSection(group: nestedGroup)
+            section.orthogonalScrollingBehavior = .continuous // no paging behavorial
+            
+            // section header
+            // steps to add a section header to a section
+            // 1. define the size and add to section
+            // 2. register
+            // 3. deuque the supplmentary view 
+            let headerSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(44))
+            let header = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: headerSize, elementKind: UICollectionView.elementKindSectionHeader, alignment: .top)
+            section.boundarySupplementaryItems = [header]
             
             return section
         }
@@ -107,6 +132,21 @@ class ViewController: UIViewController {
             return cell
         })
         
+        // deqeueu the header supplementaty view
+        dataSource.supplementaryViewProvider = {
+            (collectionView, kind, indexPath) in
+            guard let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: HeaderView.reuseIdentifier, for: indexPath) as? HeaderView, let sectionKind = SectionKind(rawValue: indexPath.section) else {
+                fatalError("could not dequeu header view")
+            }
+            
+            // congure header view
+            headerView.textLabel.text = sectionKind.sectionTitle
+            headerView.textLabel.textAlignment = .left
+//            headerView.textLabel.font =
+            
+            return headerView
+        }
+        
         // create initial snapshot
         var snapshot = NSDiffableDataSourceSnapshot<SectionKind, Int>()
         snapshot.appendSections([.first, .second, .third])
@@ -115,11 +155,12 @@ class ViewController: UIViewController {
         
         snapshot.appendItems(Array(1...20), toSection: .first)
         snapshot.appendItems(Array(21...40), toSection: .second)
-        snapshot.appendItems(Array(41...60), toSection: .first)
+        snapshot.appendItems(Array(41...60), toSection: .third)
         
         dataSource.apply(snapshot, animatingDifferences: false)
     }
     
 
 }
+
 
