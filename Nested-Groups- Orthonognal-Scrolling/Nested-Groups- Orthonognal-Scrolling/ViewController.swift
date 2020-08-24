9def5aed7245d31ef4a7f89c1fc492acdae890c8
Ameni Alsaydi <amenialsaydi@pursuit.org>
2020-08-24 11:10:56 -0400

    collection view appears with cells

--- Nested-Groups- Orthonognal-Scrolling/Nested-Groups- Orthonognal-Scrolling/ViewController.swift
+++ Nested-Groups- Orthonognal-Scrolling/Nested-Groups- Orthonognal-Scrolling/ViewController.swift
@@ -22,6 +22,15 @@ enum SectionKind: Int, CaseIterable {
             return 1
         }
     }
+    
+    var nestedGroupHeight: NSCollectionLayoutDimension {
+        switch self {
+        case .first:
+            return .fractionalWidth(1.0)
+        default:
+            return .fractionalWidth(0.5)
+        }
+    }
 }
 
 class ViewController: UIViewController {
@@ -30,11 +39,24 @@ class ViewController: UIViewController {
     
     typealias DataSource = UICollectionViewDiffableDataSource<SectionKind, Int>
 
+    var dataSource: DataSource!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureCollectionView()
+        configureDataSource()
     }
 
+    private func configureCollectionView() {
+        // collectionView.collectionViewLayout
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createLayout())
+        collectionView.backgroundColor = .systemGray
+        collectionView.register(LabelCell.self, forCellWithReuseIdentifier: LabelCell.reuseIdentifier)
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.addSubview(collectionView)
+    }
+    
+    
     // will create the layout for the collection view
     private func createLayout() -> UICollectionViewLayout {
         // item -> group -> section -> layout
@@ -47,7 +69,7 @@ class ViewController: UIViewController {
             
             // figure out what section we are dealing with
             guard let sectionKind = SectionKind(rawValue: sectionIndex) else {
-                return
+                fatalError("could not get section kind")
             }
             
             
@@ -57,12 +79,45 @@ class ViewController: UIViewController {
             
             // group
             let innerGroupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.50), heightDimension: .fractionalHeight(1.0))
-            let innerGroup = NSCollectionLayoutGroup.vertical(layoutSize: innerGroupSize, subitem: item, count: <#T##Int#>)
+            let innerGroup = NSCollectionLayoutGroup.vertical(layoutSize: innerGroupSize, subitem: item, count: sectionKind.itemCount )
+            
+            let nestedGroupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: sectionKind.nestedGroupHeight) // we want square so we want full width of 1/2 width
+            let nestedGroup = NSCollectionLayoutGroup.horizontal(layoutSize: nestedGroupSize, subitems: [innerGroup])
             
             // section
+            let section = NSCollectionLayoutSection(group: nestedGroup)
+            
+            return section
         }
         
-        return layou
+        return layout
+    }
+    
+    private func configureDataSource() {
+        dataSource = DataSource(collectionView: collectionView, cellProvider: { (collectionView,indexPath, item) -> UICollectionViewCell? in
+            
+             // configure cell and return cell
+            
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: LabelCell.reuseIdentifier, for: indexPath) as? LabelCell else {
+                fatalError("could not get downcast to label cell ")
+            }
+            cell.textLabel.text = "\(item)"
+            cell.backgroundColor = .orange
+            cell.layer.cornerRadius = 10
+            return cell
+        })
+        
+        // create initial snapshot
+        var snapshot = NSDiffableDataSourceSnapshot<SectionKind, Int>()
+        snapshot.appendSections([.first, .second, .third])
+        
+        // populate the section (3)
+        
+        snapshot.appendItems(Array(1...20), toSection: .first)
+        snapshot.appendItems(Array(21...40), toSection: .second)
+        snapshot.appendItems(Array(41...60), toSection: .first)
+        
+        dataSource.apply(snapshot, animatingDifferences: false)
     }
     
 
