893f8b5581fa02ed2efddf070bb13ad7a3ea9f9c
Ameni Alsaydi <amenialsaydi@pursuit.org>
2020-08-25 12:48:26 -0400

    added config file

---
+++ Compositional-Layout-with-Combine/Compositional-Layout-with-Combine/Config.swift
@@ -0,0 +1,13 @@
+//
+//  Config.swift
+//  Compositional-Layout-with-Combine
+//
+//  Created by Amy Alsaydi on 8/25/20.
+//  Copyright © 2020 Amy Alsaydi. All rights reserved.
+//
+
+import Foundation
+
+struct Config {
+  static let apikey = "API KEY GOES HERE FOR THE PIXABAY API"
+}
